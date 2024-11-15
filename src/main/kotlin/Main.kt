c89c25335a38a98e46e37ac2fa94f8bcb9f26e58
Navarro <joseanp0202@gmail.com>
2024-11-15 09:19:44 +0000

    Texto reutilizable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,13 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -19,22 +17,34 @@ import androidx.compose.ui.window.application
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    //var text by remember { mutableStateOf("Hello, World!") }
+    val textos = listOf<String>(
+        "Que dices tio",
+        "Manolito vende calamares",
+        "Carmelo se dio de ostias con lorenzo",
+        "Manolo lama comentando este partido de TFT"
+    )
 
     MaterialTheme {
-        Column() {
-            Text(
-                text = "Que dice niño",
-                modifier = Modifier
-                    .padding(24.dp)
-                    .background(color = Color.Blue)
-            )
-            Button(onClick = {
-                text = "Hello, Desktop!"
-            }) {
-                Text(text)
+        Column(modifier = Modifier.fillMaxSize()) {
+            textos.forEach { texto ->
+                caja(texto)
             }
         }
+    }
+}
+
+@Composable
+fun caja(texto : String) {
+    Row(modifier = Modifier
+        .background(color = Color.Blue)
+        .fillMaxWidth()
+    ) {
+        Text(
+            text = texto,
+            modifier = Modifier
+                .padding(10.dp)
+        )
     }
 }
 
