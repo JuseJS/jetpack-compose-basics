fbf82c0f7f405ea808c2790ec275e2138cf5ad65
Navarro <joseanp0202@gmail.com>
2024-12-08 11:09:03 +0000

    Retoques finales

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,74 +1,34 @@
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
-fun App() {
-    val textos = List(50) { index ->
-        val color = if (index % 2 == 0) Color.Red else Color.Yellow
-        "Elemento número $index" to color
-    }
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
 
     MaterialTheme {
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items(textos.size) { index ->
-                val texto = textos[index]
-                Caja(texto.first, texto.second)
+        Surface(modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            if (shouldShowOnboarding) {
+                OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+            } else {
+                Greetings()
             }
-        }
-    }
-}
-
-@Composable
-fun Caja(texto: String, bgColor: Color = Color.Unspecified) {
-    var isExtended by remember { mutableStateOf(false) }
-    val rowPadding by animateDpAsState(
-        targetValue = if (isExtended) 50.dp else 24.dp,
-        animationSpec = tween(durationMillis = 500)
-    )
-
-    Row(
-        modifier = Modifier
-            .background(color = bgColor)
-            .fillMaxWidth()
-            .padding(bottom = rowPadding),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 24.dp, top = 24.dp)
-        ) {
-            Text(
-                text = texto,
-                color = Color.White
-            )
-        }
-        Button(
-            onClick = {
-                isExtended = !isExtended
-            },
-            modifier = Modifier
-                .padding(end = 24.dp, top = 24.dp)
-        ) {
-            Text(text = if (isExtended) "Mostrar Menos" else "Mostrar más")
         }
     }
 }
@@ -83,25 +43,84 @@ fun OnboardingScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("¡Bienvenido al Codelab!")
+        Text("Bienvenido al CodeLab!")
         Button(
-            modifier = Modifier
-                .padding(vertical = 24.dp),
+            modifier = Modifier.padding(vertical = 24.dp),
             onClick = onContinueClicked
         ) {
-            Text("Continuar")
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hola, ")
+            Text(
+                text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Asta grita en medio del caos mágico: \"¡Nunca me rendiré!\" Sin poseer magia, pero con un espíritu indomable, entrena día tras día enfrentándose a cualquier desafío. Su grimorio negro, portador de la anti-magia, siempre está listo, y su rival y amigo Yuno lo impulsa a superarse. \"¡Fuerza y coraje!\" exclama mientras empuña la espada que anula cualquier hechizo.\n" +
+                            "\n" +
+                            "En el Reino del Trébol, atrapado por su sueño de convertirse en el Rey Mago, lucha contra todas las probabilidades. Las batallas son intensas, la camaradería con los Black Bulls es inquebrantable, y la justicia arde como un fuego en su interior. Asta proclama con determinación: \"¡Todo lo imposible se puede lograr!\"\n" +
+                            "\n" +
+                            "Al final, la unidad y la determinación son el camino hacia la victoria. Pero, ¿quién puede predecir qué le depara el futuro a este héroe sin magia, pero con un corazón lleno de esperanza?")
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) "Show less" else "Show more"
+            )
         }
     }
 }
 
 fun main() = application {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
-
     Window(onCloseRequest = ::exitApplication) {
-        if (shouldShowOnboarding) {
-            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            App()
-        }
+        MyApp(Modifier.fillMaxSize())
     }
 }
